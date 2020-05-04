d448320bcde2cd2269991d0cdd538d2bdc266ddb
Ulises López <ulises050794@gmail.com>
2020-05-03 22:11:37 -0600

    starting aggregatin planning

---
+++ src/main/java/controller/AggregatePlanning.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class AggregatePlanning {
+
+    public static void getPersuitnStrategy () {
+
+    }
+
+    public static void getLevelForceWithOvertime () {
+
+    }
+
+    public static void getLevelForceWithOutsourcing () {
+
+    }
+
+    public static void getPerMothType () {
+
+    }
+}
